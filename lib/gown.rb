468fab126a0996bacbfeb93080205a079a1ae86f
dylanlacey <github@dylanlacey.com>
2018-11-20 20:51:01 +1000

    Allow gown to be called directly, not just with files.

--- lib/gown.rb
+++ lib/gown.rb
@@ -5,7 +5,7 @@ require "tty-spinner"
 module Gown
 
   class << self
-    def strip_file input_filename, output_filename
+    def patterns
       patterns_to_remove = (
         Gown::Patterns::KGP_lifecycle + 
         Gown::Patterns::KGP_warnings +
@@ -14,6 +14,31 @@ module Gown
         Gown::Patterns::CHANNEL_events +
         Gown::Patterns::MAIN_lifecycle
       ).map {|pattern| Regexp.new (Gown::Patterns::Timestamp + pattern)}
+
+      return patterns_to_remove
+    end
+
+    def strip input
+      patterns_to_remove = patterns
+      output = ""
+
+      input.each do |line|
+      if line.match /^[[:space:]]*$/
+        # blank_lines_removed +=1
+      else
+        filtered = patterns_to_remove.find { |pattern| pattern.match line }
+        if !filtered
+          output.append line
+        else
+          # lines_removed+= 1
+        end
+      end
+
+      return output
+    end
+
+    def strip_file input_filename, output_filename
+      patterns_to_remove = patterns
 
       puts "Checking for #{patterns_to_remove.length} patterns"
 
